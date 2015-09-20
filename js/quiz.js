28415825df3faad6e9ac028608126b7097dad723
Dineile <dineileguevara@hotmail.com>
2015-09-20 16:00:27 -0400

    quiz

---
+++ js/quiz.js
@@ -0,0 +1,28 @@
+/**
+ * Created by Dineile on 9/19/2015.
+ */
+
+function checkAnswers(){
+
+    var count=0;
+	var arr= ["A","B","D","B","D","A","C","A","D","A"];
+	for (var a=1;a<=arr.length;a++){
+		x=document.getElementsByName("q"+a.toString());
+		var ans;
+		for (var b=0;b<x.length;b++){
+			if (x[b].checked==true){
+				ans=x[b].value;
+				console.log(ans);
+			}
+		}
+		if (ans===arr[a-1]){
+			count++;
+		}
+	}	
+	alert ("You got "+count.toString()+" correct.");
+	return false;
+}
+
+document.addEventListener('DOMContentLoaded', function() {
+    alert ("Welcome. You have 2 minutes to complete the quiz. Click OK to begin.")
+}, false);
